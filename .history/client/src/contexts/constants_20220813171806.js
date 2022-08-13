3b5a4c8f17a416fa81347f733a07508b02fa4036
tran thanh tu <tttuab@gmail.com>
2022-08-13 17:59:45 +0700

    first commit

--- .history/client/src/contexts/constants_20220813171806.js
+++ .history/client/src/contexts/constants_20220813171806.js
@@ -1,7 +1,7 @@
 export const apiUrl =
     process.env.NODE_ENV !== 'production'
-        ? 'http://localhost:5000/api'
-        : 'https://lit-brook-37615.herokuapp.com/api'
+        ? ' https://lit-brook-37615.herokuapp.com/api'
+        : 'somedeployedURL'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern'
 
--- .history/client/src/contexts/constants_20220813172200.js
+++ .history/client/src/contexts/constants_20220813172200.js
@@ -12,5 +12,5 @@ export const POST_LOADED_FAIL = 'POST_LOADED_FAIL'
 export const ADD_POST = 'ADD_POST'
 export const DELETE_POST = 'DELETE_POST'
 export const UPDATE_POST = 'UPDATE_POST'
-export const FIND_POST = 'FIND_POST'
+export const UPDATE_POST = 'UPDATE_POST'
 
